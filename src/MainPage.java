5b94be7417eb76501374925691b710b1642bf4c9
Omar Amin <omar_75@hotmail.dk>
2020-08-06 23:13:58 +0200

    Added pie chart with effects

--- src/MainPage.java
+++ src/MainPage.java
@@ -4,9 +4,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.hibernate.Session;
@@ -26,6 +28,8 @@ public class MainPage {
     private ImageView editBalance;
     @FXML
     private TextField balance;
+    @FXML
+    private Label caption;
 
     @FXML
     private void initialize(){
@@ -34,10 +38,24 @@ public class MainPage {
     }
 
     private void setupChart(){
-        ObservableList<PieChart.Data> data = FXCollections.observableArrayList();
-        data.add(new PieChart.Data("Unspent money",Controller.user.getIncome()-Controller.user.getMonthlyPayment()));
-        data.add(new PieChart.Data("Payments",Controller.user.getMonthlyPayment()));
+        ObservableList<PieChart.Data> data = FXCollections.observableArrayList(
+                new PieChart.Data("Unspent money",Controller.user.getIncome()-Controller.user.getMonthlyPayment()),
+                new PieChart.Data("Payments",Controller.user.getMonthlyPayment())
+        );
         overview.setData(data);
+        overview.setLabelsVisible(false);
+        overview.setLegendVisible(false);
+        caption.setTextFill(Paint.valueOf("#30CA80"));
+        caption.setText("Money left this month: $ " + (Controller.user.getIncome()-Controller.user.getMonthlyPayment()));
+
+        data.get(0).getNode().addEventHandler(MouseEvent.ANY, (e) -> {
+            caption.setText("Money left this month: $ " + (Controller.user.getIncome()-Controller.user.getMonthlyPayment()));
+            caption.setTextFill(Paint.valueOf("#30CA80"));
+        });
+        data.get(1).getNode().addEventHandler(MouseEvent.ANY, (e) -> {
+            caption.setText("Money spent this month: $ " + Controller.user.getMonthlyPayment());
+            caption.setTextFill(Paint.valueOf("#ff4a4a"));
+        });
     }
 
     public void closeApp(MouseEvent mouseEvent) {
